332acdfd597d4fce42ebf07b2911264dbfd4b1d3
LZH <13466083337@163.com>
2021-07-27 15:26:12 +0800

    optimize detail and use tree for search in studentManage

--- src/layuiadmin/modules/search.js
+++ src/layuiadmin/modules/search.js
@@ -1,16 +1,57 @@
 const sbt = document.getElementById("searchBt");
 let cdb = document.getElementById("condition");
-let conditionData = {};
+let conditionData = {
+    id: "",
+    name: "",
+    sex: "",
+    pid: "",
+    class: []
+};
+var classData = [];
 
 sbt.addEventListener("click", function () {
     cdb.style.display = 'block'
 });
 
 function searchSt() {
-    layui.use('form', function () {
+    layui.use(['form', 'layer'], function () {
         var form = layui.form;
+        var layer = layui.layer;
 
-        conditionData = form.val("conditionForm");
+        let data = form.val("conditionForm");
+
+        // 处理性别数据
+        if(data.woman && data.man) {
+            conditionData.sex = "all"
+        } else if (data.woman) {
+            conditionData.sex = "woman"
+        } else if (data.man) {
+            conditionData.sex = "man"
+        }
+
+        // 处理学校、年级、班级数据
+        if(classData.length > 0) {
+            classData.forEach(function(item) {
+                // if(item.level == 1) {
+                //     conditionData.schoool.push(item.id);
+                // } else if(item.level == 2) {
+                //     conditionData.grade.push(item.id);
+                // } else if(item.level == 3) {
+                //     conditionData.class.push(item.id);
+                // }
+                if(item.leaf) {
+                    conditionData.class.push(item.nodeId);
+                }
+            })
+        }
+
+        // 填入其他数据
+        for(let key in data) {
+            if(key == 'man' || key == 'woman') {
+                continue;
+            }
+            conditionData[key] = data[key];
+        } 
         console.log(conditionData);
     });
     reLoadTable();
