605c55f333961e43013b059808092ecfc7bebaf6
LZH <13466083337@163.com>
2021-07-28 17:50:16 +0800

    finishi delete of paymentManage

--- src/layuiadmin/modules/delete.js
+++ src/layuiadmin/modules/delete.js
@@ -1,34 +1,68 @@
 let deleteBt = document.getElementById("deleteBt");
 
 deleteBt.addEventListener("click", function () {
-    layui.use('layer', function () {
-        var layer = layui.layer;
+    if (deleteBt.innerText.indexOf('班级') >= 0) {
+        layui.use('layer', function () {
+            var layer = layui.layer;
 
-        if (classData.length < 1) {
-            layer.msg("请至少选择一个有效班级");
-            return;
-        }
+            if (classData.length < 1) {
+                layer.msg("请至少选择一个有效班级");
+                return;
+            }
 
-        // 对班级信息数据做预处理，只留下班级
-        let onlyClass = classData.filter(function(item) {
-            return item.leaf
+            // 对班级信息数据做预处理，只留下班级
+            let onlyClass = classData.filter(function (item) {
+                return item.leaf
+            })
+
+            layer.open({
+                type: 1,
+                content: '确定删除选中的 ' + onlyClass.length + '个班级吗？',
+                skin: 'delete-confirm',
+                area: ['300px', '200px'],
+                btn: ['确定', '取消'],
+                yes: function (index, layero) {
+                    console.log('确定删除');
+                    reloadTree();
+                    layer.close(index);
+                },
+                btn2: function (index, layero) {
+                    console.log('取消删除');
+                    layer.close(index);
+                },
+            });
         })
+    } else if (deleteBt.innerText.indexOf('模板') >= 0) { 
+        let checkStatus = {},
+            validate = false;
+        layui.use(['layer', 'table'], function () {
+            var layer = layui.layer;
+            var table = layui.table;
 
-        layer.open({
-            type: 1,
-            content: '确定删除选中的 ' + onlyClass.length + '个班级吗？',
-            skin: 'delete-confirm',
-            area: ['300px', '200px'],
-            btn: ['确定', '取消'],
-            yes: function (index, layero) {
-                console.log('确定删除');
-                reloadTree();
-                layer.close(index);
-            },
-            btn2: function (index, layero) {
-                console.log('取消删除');
-                layer.close(index);
-            },
-        });
-    })
+            checkStatus = table.checkStatus('idTest')
+            
+            if(checkStatus.data.length < 1) {
+                layer.msg('请至少选择一个有效模板');
+                return;
+            }
+            validate = true;
+
+            layer.open({
+                type: 1,
+                content: '确定删除选中的 ' + checkStatus.data.length + '个模板吗？',
+                skin: 'delete-confirm',
+                area: ['300px', '200px'],
+                btn: ['确定', '取消'],
+                yes: function (index, layero) {
+                    console.log('确定删除');
+                    reloadTable();
+                    layer.close(index);
+                },
+                btn2: function (index, layero) {
+                    console.log('取消删除');
+                    layer.close(index);
+                },
+            });
+        })
+    }
 })
